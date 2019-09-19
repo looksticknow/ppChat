3e8391433426a3dd3ac145e2bd21a5bdb769b8a1
Jorgsaa <jorgen.saanum@gmail.com>
2019-09-19 18:41:19 +0200

    Test commit

--- chatClient.py
+++ chatClient.py
@@ -18,6 +18,7 @@ class Client:
 
     def __init__(self, address):
 
+        print("Initializing")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.connect((address, 32555))
