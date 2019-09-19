9afbdde013d6f6f0e396f03350f702eb3f5122c0
Jorgsaa <jorgen.saanum@gmail.com>
2019-09-19 18:43:38 +0200

    Revert "Test commit"
    This reverts commit 3e839143

--- chatClient.py
+++ chatClient.py
@@ -18,7 +18,6 @@ class Client:
 
     def __init__(self, address):
 
-        print("Initializing")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.connect((address, 32555))
